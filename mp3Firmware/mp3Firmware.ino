869751eded90e4ea0c4ce18e40454c85d69211d1
hurricaneJoef <hurricane.joef@gmail.com>
2022-09-30 10:48:31 -0400

    init commit

---
+++ mp3Firmware/mp3Firmware.ino
@@ -0,0 +1,15 @@
+#define left_pin A
+#define right_pin A
+#define thresh_on 525
+#define thresh_off 500
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
